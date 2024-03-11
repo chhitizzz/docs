95a252b7fcc4013f262a680c271d0d9d84bb155e
ChhitizV <chhitizvaidya10@gmail.com>
2024-03-11 11:04:37 +0545

    cards

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Foreground() {
   return (
     <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'></div>
     </div>
   )
 }
