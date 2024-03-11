1e19d2d66846af9923d76f6c326c0e1030bedbe5
ChhitizV <chhitizvaidya10@gmail.com>
2024-03-11 10:54:21 +0545

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Background from './components/Background';
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none tracking-tighter font-thin text-zinc-200'>Docs.</h1>
+      <Background/>
     </div>
   )
 }
