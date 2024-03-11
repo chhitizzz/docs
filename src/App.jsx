1d4b5b013f8fb679ef561251fdf93ef7ddbf1253
ChhitizV <chhitizvaidya10@gmail.com>
2024-03-11 10:44:27 +0545

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none tracking-tighter'>Docs</h1>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[14vw] leading-none tracking-tighter font-thin text-zinc-200'>Docs.</h1>
     </div>
   )
 }
