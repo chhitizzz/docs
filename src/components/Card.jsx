3d9031cef8b124112a31bce5f1db16fee775ae82
ChhitizV <chhitizvaidya10@gmail.com>
2024-03-12 22:15:08 +0545

    icons and paragraph

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {FaRegFileAlt} from 'react-icons/fa';
 
 function Card() {
   return (
-    <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90'>
+      <div className='relative w-60 h-72 rounded-[20px] bg-zinc-900/90 text-white p-5'>
+        <FaRegFileAlt/>
+        <p className='text-xs mt-5 font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, optio?
+        </p>
     </div>
   )
 }
